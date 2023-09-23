64cf42234e0e44f268a32bfe4a7408481cf128dc
Arun Kumar C <arunkumar.chinnasamy@mallow-tech.com>
2023-09-23 13:24:05 +0530

    about added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  resources :home do
+    get :about, on: :collection
+  end
+  root "home#index"
 end
